d1e2213973d1b3258b6efbeb8eedacd32f3e7683
Adam NAILI <a.naili@live.fr>
2022-02-15 16:49:28 +0100

    feat: ends the chapter 14 just before the challenge

--- CriminalIntent-Challenge5-MoreDialogs/app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListViewModel.kt
+++ CriminalIntent-Challenge5-MoreDialogs/app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListViewModel.kt
@@ -5,4 +5,8 @@ import androidx.lifecycle.ViewModel
 class CrimeListViewModel : ViewModel() {
     private val crimeRepository = CrimeRepository.get()
     val crimeListLiveData = crimeRepository.getCrimes()
+
+    fun addCrime(crime: Crime) {
+        crimeRepository.addCrime(crime)
+    }
 }
