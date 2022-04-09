387fb2982a9dc4dbe3f1a6562cc0aff694cb8e85
Adam NAILI <a.naili@live.fr>
2022-04-09 09:52:18 +0200

    feat: initialize the challenge of chapter 17

---
+++ CriminalIntent-Challenge10-LocalizingDates/app/src/main/java/com/bignerdranch/android/criminalintent/Crime.kt
@@ -0,0 +1,17 @@
+package com.bignerdranch.android.criminalintent
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import java.util.*
+
+@Entity
+data class Crime(
+    @PrimaryKey var id: UUID = UUID.randomUUID(),
+    var title: String = "",
+    var date: Date = Date(),
+    var isSolved: Boolean = false,
+    var suspect: String = ""
+) {
+    val photoFileName
+        get() = "IMG_$id.jpg"
+}
