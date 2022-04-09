ab157109004a5963d1206de5dfdea01a7b0e04ba
Adam NAILI <a.naili@live.fr>
2022-04-09 09:14:11 +0200

    feat: finish challenge 9 on efficient thumbnail load

---
+++ CriminalIntent-Challenge9-EfficientThumbnailLoad/app/src/main/java/com/bignerdranch/android/criminalintent/CrimeDetailViewModel.kt
@@ -0,0 +1,29 @@
+package com.bignerdranch.android.criminalintent
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Transformations
+import androidx.lifecycle.ViewModel
+import java.io.File
+import java.util.*
+
+class CrimeDetailViewModel : ViewModel() {
+    private val crimeRepository: CrimeRepository = CrimeRepository.get()
+    private val crimeIdLiveData = MutableLiveData<UUID>()
+
+    var crimeLiveData: LiveData<Crime?> = Transformations.switchMap(crimeIdLiveData) {
+        crimeRepository.getCrime(it)
+    }
+
+    fun loadCrime(crimeId: UUID) {
+        crimeIdLiveData.value = crimeId
+    }
+
+    fun saveCrime(crime: Crime) {
+        crimeRepository.updateCrime(crime)
+    }
+
+    fun getPhotoFile(crime: Crime): File {
+        return crimeRepository.getPhotoFile(crime)
+    }
+}
---
+++ CriminalIntent-Challenge9-EfficientThumbnailLoad/app/src/main/java/com/bignerdranch/android/criminalintent/CriminalIntentApplication.kt
@@ -0,0 +1,10 @@
+package com.bignerdranch.android.criminalintent
+
+import android.app.Application
+
+class CriminalIntentApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        CrimeRepository.initialize(this)
+    }
+}
