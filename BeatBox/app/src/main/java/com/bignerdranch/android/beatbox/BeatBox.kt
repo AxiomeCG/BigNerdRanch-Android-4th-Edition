746193f6216de4cb7ac73509ce18b108ced33b19
Adam NAILI <a.naili@live.fr>
2022-04-14 18:06:48 +0200

    feat: finish chapter 19

---
+++ BeatBox/app/src/main/java/com/bignerdranch/android/beatbox/BeatBox.kt
@@ -0,0 +1,33 @@
+package com.bignerdranch.android.beatbox
+
+import android.content.res.AssetManager
+import android.util.Log
+import java.lang.Exception
+
+private const val TAG = "BeatBox"
+private const val SOUNDS_FOLDER = "sample_sounds"
+
+class BeatBox(private val assets: AssetManager) {
+    val sounds: List<Sound>
+
+    init {
+        sounds = loadSounds()
+    }
+
+    private fun loadSounds(): List<Sound> {
+        val soundNames: Array<String>
+        try {
+            soundNames = assets.list(SOUNDS_FOLDER)!!
+        } catch (e: Exception) {
+            Log.e(TAG, "Could not list assets", e)
+            return emptyList()
+        }
+        val sounds = mutableListOf<Sound>()
+        soundNames.forEach { fileName ->
+            val assetPath = "$SOUNDS_FOLDER/$fileName"
+            val sound = Sound(assetPath)
+            sounds.add(sound)
+        }
+        return sounds
+    }
+}
